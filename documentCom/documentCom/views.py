17e06be7b30cc0a08e3c59072a8811593b7a7f70
RamssesMH <your_user_name@domain_name.comname = RamssesMH>
2022-07-07 11:52:51 -0500

    proyecto de llenado pdf

---
+++ documentCom/documentCom/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, redirect
+from django.template import Template, Context
+from django.shortcuts import render, redirect
+import os
+from django.conf import settings
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template.loader import get_template
+from xhtml2pdf import pisa
+from django.contrib.staticfiles import finders
+
+def index(request):
+    # t = 'index.html'
+    try:
+        template = get_template("plantillas/doc.html")
+        context = {'title': 'mi primer pdf'}
+        html = template.render(context)
+        response = HttpResponse(content_type='application/pdf')
+        response['Content-Disposition'] = 'attachment; filename="report.pdf"'
+        # create a pdf
+        pisa_status = pisa.CreatePDF(
+        html, dest=response)
+        # if error then show some funny view
+        if pisa_status.err:
+            return HttpResponse('We had some errors <pre>' + html + '</pre>')
+        return response
+    except:
+        pass
+    HttpResponseRedirect("formulario/")
+    # return render(request, t)
