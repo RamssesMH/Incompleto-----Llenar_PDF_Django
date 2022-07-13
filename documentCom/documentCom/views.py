add3a93924b786f523904c033ad6daaa4e183b61
RamssesMH <your_user_name@domain_name.comname = RamssesMH>
2022-07-13 15:00:08 -0500

    Subiendo cambios de views y arhivo html que genera PDF

--- documentCom/documentCom/views.py
+++ documentCom/documentCom/views.py
@@ -9,21 +9,33 @@ from xhtml2pdf import pisa
 from django.contrib.staticfiles import finders
 
 def index(request):
-    # t = 'index.html'
-    try:
-        template = get_template("plantillas/doc.html")
-        context = {'title': 'mi primer pdf'}
-        html = template.render(context)
-        response = HttpResponse(content_type='application/pdf')
-        response['Content-Disposition'] = 'attachment; filename="report.pdf"'
-        # create a pdf
-        pisa_status = pisa.CreatePDF(
-        html, dest=response)
-        # if error then show some funny view
-        if pisa_status.err:
-            return HttpResponse('We had some errors <pre>' + html + '</pre>')
-        return response
-    except:
-        pass
-    HttpResponseRedirect("formulario/")
-    # return render(request, t)
+    t = 'index.html'
+
+
+
+    if request.method == "POST":
+
+        usuariop = request.POST["usuario"]
+        nombrep = request.POST["nombre"]
+        matriculap = request.POST["matricula"]
+        RFCp = request.POST["rfc"]
+
+        try:
+            template = get_template("plantillas/doc.html")
+            context = {'usuario': usuariop, 'nombre': nombrep, 'matricula': matriculap, 'rfc': RFCp}
+            html = template.render(context)
+            response = HttpResponse(content_type='application/pdf')
+            # response['Content-Disposition'] = 'attachment; filename="report.pdf"'
+            # create a pdf
+            pisa_status = pisa.CreatePDF(
+            html, dest=response)
+            # if error then show some funny view
+            if pisa_status.err:
+                return HttpResponse('We had some errors <pre>' + html + '</pre>')
+            return response
+        except:
+            pass
+        HttpResponseRedirect("formulario/")
+
+        
+    return render(request, t)
